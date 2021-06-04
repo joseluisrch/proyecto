b5277cde06726f61926422dc5b5f05bf7ce289b8
joseluisrch <82129903+joseluisrch@users.noreply.github.com>
2021-06-04 04:35:31 -0500

    Add files via upload

---
+++ proyecto/conexion.php
@@ -0,0 +1,12 @@
+<?php 
+	
+	$host = 'localhost';
+	$user = 'root';
+	$password = '';
+	$db = 'facturacion';
+
+	$conection = @mysqli_connect($host,$user,$password,$db);
+	//mysqli_close($conection);
+	if(!$conection){
+		echo "Error en la conexión";
+	}
